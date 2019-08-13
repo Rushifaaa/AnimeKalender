138cf40988c40e3a573d6716d1dd52ab464b9489
Jakub Gencer <jakub.gencer@allaboutapps.at>
2019-08-13 11:48:13 +0200

    Web Scraping function for the Calender

---
+++ app/src/main/java/com/example/sitescraping/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.sitescraping
+
+import org.jsoup.Jsoup
+
+fun main() {
+    val doc = Jsoup.connect("https://confluence.allaboutapps.at/#all-updates").get()
+    doc.run {
+        select("body").forEachIndexed { index, element ->
+            val list = element.select("ul.list-container list-container-all-updates")
+            val selected = list.select("h4")
+            println("$index. ${element.text()}, $selected")
+            //println("$img")
+        }
+        print("test")
+    }
+    //println(doc)
+
+}
